60fbd9c7c7c9664f75bf4a9ace8ca46f59672aed
muraterennar <muraterennar@hotmail.com>
2023-10-10 20:26:33 +0300

    Global Excetion Handler

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Core.Application.Rules;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -9,11 +10,30 @@ public static class ApplicationServiceRegistration
     {
 
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        // Burada Sistemime eklediğimiz Herbir Business Rules (İş Kurallarını) teker Teker eklemkten kaçınıp
+        // Tipini verdiğimiz Türün Assembly Eklemimizi Sağlıyor.
+        services.AddSubClassedOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
 
+
+
+        return services;
+    }
+
+    // Tipini verdiğimiz Türün Assembly Eklemimizi Sağlayan Fonksiyon.
+    public static IServiceCollection AddSubClassedOfType(this IServiceCollection services, Assembly assembly, Type type, Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+            else
+                addWithLifeCycle(services, type);
 
 
         return services;
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Application;
+using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseHttpsRedirection();
 
